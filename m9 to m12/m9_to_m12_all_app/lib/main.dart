b0b19231347c8b9789e4199abc2dd5c07cf4ef07
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-10-07 00:39:43 +0600

    module9 liveTest

--- m9 to m12/m9_to_m12_all_app/lib/main.dart
+++ m9 to m12/m9_to_m12_all_app/lib/main.dart
@@ -1,19 +1,73 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-import 'home_screen.dart';
-
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Select Sizer'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ButtonList(),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonList extends StatefulWidget {
+  @override
+  _ButtonListState createState() => _ButtonListState();
+}
+
+class _ButtonListState extends State<ButtonList> {
+  Map<String, Color> buttonColors = {
+    'S': Colors.blue,
+    'M': Colors.blue,
+    'L': Colors.blue,
+    'XL': Colors.blue,
+    'XXL': Colors.blue,
+    'XXXL': Colors.blue,
+  };
+
+  void showSnackbar(String size) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected size: $size'),
+      ),
+    );
+  }
+
+  void changeColor(String size) {
+    setState(() {
+      buttonColors[size] = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 20,
+      children: buttonColors.keys.map((size) {
+        return ElevatedButton(
+          onPressed: () {
+            showSnackbar(size);
+            changeColor(size);
+          },
+          child: Text(size),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(buttonColors[size]!),
+          ),
+        );
+      }).toList(),
     );
   }
 }
